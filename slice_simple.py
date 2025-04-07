c966e61f680d168f6d43f68b0fc4c9abfa8aab8f
abril1208 <arabinovich@mail.austral.edu.ar>
2025-04-07 11:50:22 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,7 +1,7 @@
 def slice_simple():
     texto = "Awesome"
     texto=texto.lower()
-    print (texto[:3])
+    print (texto[:3]) 
     print(texto[int(len(texto)/2)-1 :int(len(texto)/2) +2 ])
     print(texto[:4]+texto[len(texto)-3:])
 slice_simple()
